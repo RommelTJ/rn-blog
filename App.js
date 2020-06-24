8c46945eea97a90fe6d0350b3abcd9dca9bda7ac
Rommel Rico <me@rommelrico.com>
2020-06-24 06:48:40 -0700

    :sparkles: State: Using BlogProvider

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
+import { BlogProvider } from "./src/context/BlogContext";
 import IndexScreen from "./src/screens/IndexScreen";
 
 const navigator = createStackNavigator(
@@ -19,5 +20,9 @@ const navigator = createStackNavigator(
 const App = createAppContainer(navigator);
 
 export default () => {
-  return <App />;
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
 };
