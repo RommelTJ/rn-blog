aa702836afac90af81ddde5b5991b9f3ebe4aeab
Rommel Rico <me@rommelrico.com>
2020-06-24 06:56:11 -0700

    :sparkles: State: Rendering a List of Posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,8 +3,14 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = (props) => {
+  const blogPosts = [
+    { title: "Blog Post 1" },
+    { title: "Blog Post 2" },
+    { title: "Blog Post 3" }
+  ];
+
   return (
-    <BlogContext.Provider value={"hi there"}>
+    <BlogContext.Provider value={blogPosts}>
       { props.children }
     </BlogContext.Provider>
   )
