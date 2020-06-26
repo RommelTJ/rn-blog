0ab94de3243723e89e9be022eb0594dfed4633f7
Rommel Rico <me@rommelrico.com>
2020-06-25 21:11:00 -0700

    :sparkles: State: Deleting Posts Reducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,6 +4,8 @@ const blogReducer = (state, action) => {
   switch (action.type) {
     case "add_blogpost":
       return [...state, { id: `${Math.floor(Math.random() * 99999)}`, title: `Blog Post #${state.length + 1}`}];
+    case "delete_blogpost":
+      return state.filter(x => x.id !== action.payload);
     default:
       return state;
   }
