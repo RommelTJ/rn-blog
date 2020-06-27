7b9c0ea94b788d8d11308eabc8abda18a5e6b586
Rommel Rico <me@rommelrico.com>
2020-06-27 02:58:27 -0700

    :sparkles: State: Initializing State from Context

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+
+import { Context as BlogContext } from "../context/BlogContext";
 
 const EditScreen = (props) => {
   const { navigation } = props;
   const id = navigation.getParam('id');
+  const { state } = useContext(BlogContext);
+  const blogPost = state.find(x => x.id === id);
+
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
+
   return (
     <View>
-      <Text>Edit Screen - {id}</Text>
+      <Text>Edit Title:</Text>
+      <TextInput value={title} onChangeText={setTitle} />
+      <Text>Edit Content:</Text>
+      <TextInput value={content} onChangeText={setContent} />
     </View>
   );
 };
